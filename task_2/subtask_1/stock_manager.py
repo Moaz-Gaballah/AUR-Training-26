d8e97af1873811c37d5860e2a64fba114b483678
Moaz Gaballah <moazforfire@gmail.com>
2026-08-23 02:10:19 +0300

    added the remaining funcs and main loop

--- task_2/subtask_1/stock_manager.py
+++ task_2/subtask_1/stock_manager.py
@@ -1,3 +1,5 @@
+PATH = "stock.txt"
+
 def open_file(path:str) -> dict:
     """
     Function to open the file safely
@@ -10,7 +12,7 @@ def open_file(path:str) -> dict:
             rows = file.readlines()
             for row in rows:
                 key_value = list(row.strip().split(","))
-                stock[key_value[0]] = int(key_value[1])
+                stock[key_value[0].strip().lower()] = int(key_value[1].strip())
 
     except FileNotFoundError:
         raise Exception("Error while opening the file")
@@ -18,6 +20,7 @@ def open_file(path:str) -> dict:
         raise Exception(f"Error occured {e}")
 
     return stock
+
 
 def menu_display() -> int:
     """
@@ -27,7 +30,7 @@ def menu_display() -> int:
     values = ('1', "2", "3", "4")
 
     while True:
-        x = input("""enter 1 to add stock
+        x = input("""\nenter 1 to add stock
 enter 2 to remove stock
 enter 3 to show stock’s contents
 enter 4 to exit the program
@@ -39,7 +42,86 @@ enter 4 to exit the program
             print("\n--Please Enter a valid number---")
 
 
+def stock_display(stock:dict) -> None:
+    """
+    Function to display stock in readable format
+    """
+    print("-" * 15)
+    for id, (key, value) in enumerate(stock.items()):
+        print(f"{id+1}. {key}: {value}")
+
+
+def key_input(stock:dict) -> str:
+    while True:
+        x = input("""Enter stock name or id
+e.g. enter “banana” or “1” to change banana stock or enter “Dates” for a new stock\n>>> """).strip()
+
+        if x.isdigit() and 1 <= int(x) <= len(stock):
+            x = int(x)
+            return list((stock.keys()))[x-1]
+        elif x.isalpha():
+            return x.lower()
+        else:
+            print("---Enter a valid input---")
+
+def value_input() -> int:
+    while True:
+        x = input("Enter the amount: ").strip()
+        if x.isdigit():
+            return int(x)
+        else:
+            print("Please Enter a valid amount")
+            
+
+
+def adding(stock:dict):
+    stock_display(stock)
+    key = key_input(stock)
+    value = value_input()
+
+    if key in stock:
+        stock[key] += value
+    else:
+        stock[key] = value
+    stock_display(stock)
+
+
+def removing(stock:dict):
+    while True:
+        stock_display(stock)
+        key = key_input(stock)
+        value = value_input()
+
+        if key not in stock:
+            print("---Please Enter a name from the stock---")
+        else:
+            if stock[key] - value < 0:
+                print("--Value is too large can't be removed enter a valid amount---")
+            else:
+                stock[key] -= value
+                break
+
+    stock_display(stock)
+
+def save_prog(stock:dict, path):
+    with open(path, 'w') as file:
+        for key, value in stock.items():
+            file.write(f"{key},{value}\n")
+
 if __name__ == "__main__":
-    menu_display()
-    
+
+    stock = open_file(PATH)
+   
+    while True:
+        inp = menu_display()
+
+        if inp == 1:
+            adding(stock)
+        elif inp == 2:
+            removing(stock)
+        elif inp == 3:
+            stock_display(stock)
+        elif inp == 4:
+            save_prog(stock, PATH)
+            break
 
